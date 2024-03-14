ecb6c9894cf01234444990f6e8e921f1cabacee3
EnjunCai <1208823155@qq.com>
2024-03-14 23:54:56 +0800

    perf:打包更新

---
+++ assets/404-cd2cb886.js
@@ -0,0 +1,1 @@
+import{d as t,b as a,c as n,T as o,o as s}from"./@vue-ee9ac256.js";const _=t({__name:"404",setup(r){const e=a("404");return(c,l)=>(s(),n("h1",null,o(e.value),1))}});export{_ as default};
